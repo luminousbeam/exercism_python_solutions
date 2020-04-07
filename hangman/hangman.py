8c884c3a0e49411565664b97c7527f6a401cb383
luminousbeam <jocelyn.jeriah@gmail.com>
2020-04-07 00:01:20 -0400

    completed Hangman exercise

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,9 +1,6 @@
-# Game status categories
-# Change the values as you see fit
 STATUS_WIN = "win"
 STATUS_LOSE = "lose"
 STATUS_ONGOING = "ongoing"
-
 
 class Hangman:
     def __init__(self, word):
@@ -14,22 +11,30 @@ class Hangman:
         self.word2 = word
 
     def guess(self, char):
+        if self.remaining_guesses==-1:
+            raise ValueError('You are out of guesses')
+        if self.get_status()=='win':
+            raise ValueError('You already Won')
+        #Find Index of Char in Word
         result = self.word2.find(char)
-        self.remaining_guesses -= 1
+        #If Char is not in Word
         if result == -1:
+            self.remaining_guesses -= 1
             self.result_check()
-            return 
-        self.word2 = self.word2[0:result] + " " + self.word2[result + 1:]
-        print(self.word2)
-        temp = self.masked_word[0:result] + char + self.masked_word[result + 1:]
-        self.masked_word = temp
-        self.result_check()
+            return
 
+        while(self.word2.find(char) != -1):
+            result = self.word2.find(char)
+            #Build new word replacing the found character with empty space
+            self.word2 = self.word2[0:result] + " " + self.word2[result + 1:]
+            #Build new masked word replacing the underscore in the correct position to the character
+            temp = self.masked_word[0:result] + char + self.masked_word[result + 1:]
+            self.masked_word = temp
+        self.result_check()
     def result_check(self):
-        print(self.remaining_guesses)
         if self.word == self.masked_word:
             self.status = STATUS_WIN
-        if self.remaining_guesses == 0:
+        if self.remaining_guesses == -1:
             self.status = STATUS_LOSE
 
     def get_masked_word(self):
@@ -37,11 +42,3 @@ class Hangman:
 
     def get_status(self):
         return self.status
-
-# game = Hangman('foo')
-# game.guess('f')
-# game.guess('o')
-# game.guess('o')
-
-# print(game.get_masked_word())
-# print(game.get_status())
